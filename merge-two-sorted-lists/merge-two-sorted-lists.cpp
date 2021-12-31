b50ddf8d5d1c6e5dc8ce0e436b6a83eb7f4603d9
Anubhav Agarwal <47800990+anubop@users.noreply.github.com>
2021-12-31 13:54:21 -0600

    Time: 15 ms (7.53%), Space: 14.9 MB (46.78%) - LeetHub

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        ListNode* ptr1 = list1;
+        ListNode* ptr2 = list2;
+        ListNode* head;
+        ListNode* curr;
+            
+        
+        //init head
+        if(ptr1 != NULL && ptr2 != NULL) {
+        //ptr 1 is less than or equal to ptr 2
+            if(ptr1->val <= ptr2->val) {
+                head = ptr1;
+                ptr1 = ptr1->next;
+            } else {
+                head = ptr2;
+                ptr2 = ptr2->next;
+            }
+        }
+        else if(ptr1 == NULL) {
+            
+            return ptr2;
+        }
+        
+        else if(ptr2 == NULL) {
+            return ptr1;
+        }
+            
+        curr = head;
+        while(ptr1 != NULL || ptr2 != NULL) {
+            //case 1 both lists are not empty
+            if(ptr1 != NULL && ptr2 != NULL) {
+                //ptr 1 is less than or equal to ptr 2
+                if(ptr1->val <= ptr2->val) {
+                    curr->next = ptr1;
+                    ptr1 = ptr1->next;
+                } else {
+                    curr->next = ptr2;
+                    ptr2 = ptr2->next;
+                }
+                curr = curr->next;
+            }
+            //case 2 list 1 is empty
+            else if(ptr1 == NULL) {
+                curr->next = ptr2;
+                return head;
+            }
+            //case 3 list 2 is empty
+            else if(ptr2 == NULL) {
+                curr->next = ptr1;
+                return head;
+            }
+        }
+        return head;
+    }
+};
